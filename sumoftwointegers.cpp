7df9736a5f056fc2fe4d79cb5f4e948edcf8f7b7
Virender-Singh99 <vpoonia0000@gmail.com>
2021-09-20 22:16:03 +0530

    sum revert

--- sumoftwointegers.cpp
+++ sumoftwointegers.cpp
@@ -17,12 +17,5 @@ int main() {
   sum = solveMeFirst(num1,num2);
   cout<< sum;
   return 0; 
-}
-int main() {
-  int num1, num2;
-  int sum;
-  cin>>num1>>num2;
-  sum = solveMeFirst(num1,num2);
-  cout<< sum;
-  return 0; 
+
 }
